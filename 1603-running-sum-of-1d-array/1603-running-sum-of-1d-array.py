894156ad3bdcbe44cc806f6feb67fb750d8fc8a3
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2023-10-03 14:07:22 +0100

    Time: 39 ms (91.53%), Space: 16.5 MB (40.27%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -4,7 +4,4 @@ class Solution:
         ## looping through the list
         for i in range(1,len(nums)):
            output.append(output[i-1] + nums[i])
-           
-        ## return the output of the loop
         return output
-        print(output)  # This will print [1,3,4,5]
